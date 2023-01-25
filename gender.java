1c00e66d704c0ddc56d8c5093df267aca00fe662
MadduMorriesMaddu <123434488+MadduMorriesMaddu@users.noreply.github.com>
2023-01-25 11:07:01 +0530

    Create gender.java

---
+++ gender.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class gender{
+  public static void main(String[] args) {
+    string m,f;
+    Scanner obj = new Scanner(System.in);
+    System.out.println("Enter  your letter :" )
+    

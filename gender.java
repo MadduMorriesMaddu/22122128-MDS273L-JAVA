6efc41f49cc1c64798f4e82c2c26b631e2bd2732
MadduMorriesMathew <morriesmaddu@gmail.com>
2023-01-25 12:47:40 +0530

    NS

--- gender.java
+++ gender.java
@@ -1,7 +1,16 @@
 import java.util.*;
 class gender{
-  public static void main(String[] args) {
-    string m,f;
-    Scanner obj = new Scanner(System.in);
-    System.out.println("Enter  your letter :" )
+  public static void main(String[] args){
+    char M,F;
+    Scanner obj=new Scanner(System.in);
+     System.out.println("Enter your Letter:");
+     M=obj.next().charAt(0);
+    F=obj.next().charAt(0);
+    if(M =='m'){
+      System.out.println("Male"); 
+    }else if(F=='f') {
+      System.out.println("Female");
+    }
+  }
+}
     
